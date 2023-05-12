0a247cf59bdd9b23db6e14ba10269a0fb098b4e3
Jakub Domoń <w69554@student.wsiz.edu.pl>
2023-05-12 10:13:20 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bookstore_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
