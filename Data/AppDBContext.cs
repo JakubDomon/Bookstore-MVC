b704472d891a5a78cc50ad5f83bd15905d72b798
Jakub Domoń <w69554@student.wsiz.edu.pl>
2023-05-15 12:45:45 +0200

    Database config

---
+++ Data/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Bookstore_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookstore_MVC.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
