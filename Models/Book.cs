88da3be2818eb5418766127b86f908e0157020a0
Jakub Domoń <w69554@student.wsiz.edu.pl>
2023-05-15 12:46:25 +0200

    Created book model /w database migration

---
+++ Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bookstore_MVC.Models
+{
+    public class Book
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public DateTime CreatedAt { get; set;}
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using Bookstore_MVC.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// SQL Server database connection
+builder.Services.AddDbContext<AppDBContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 
 var app = builder.Build();
 
