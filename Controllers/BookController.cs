c0f33652566f4dc0ef56f0b1a74ef6025d7311d9
Jakub Domoń <w69554@student.wsiz.edu.pl>
2023-05-15 15:23:52 +0200

    Layout

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,11 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bookstore_MVC.Data;
+using Bookstore_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bookstore_MVC.Controllers
 {
     public class BookController : Controller
     {
+        private readonly AppDBContext _db;
+
+        public BookController(AppDBContext database)
+        {
+            _db = database;
+        }
+
         public IActionResult Index()
         {
+            IEnumerable<Book> objBooksList = _db.Books.ToList();
+            return View(objBooksList);
+        }
+
+        // GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Book book)
+        {
+            _db.Books.Add(book);
+            _db.SaveChanges();
             return View();
         }
     }
